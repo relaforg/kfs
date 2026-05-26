31b81c1e4ca7f8fd1d122202eef712023f7a22b9
relaforg <remi.laforgue@gmail.com>
2026-05-26 16:48:02 +0200

    Fix gtp location + more comments on gdt asm loader

--- src/arch/x86/asm/gdt_flush.s
+++ src/arch/x86/asm/gdt_flush.s
@@ -2,17 +2,22 @@
 .type gdt_flush, @function
 
 gdt_flush:
-    movl 4(%esp), %eax
-    lgdt (%eax)
+    movl 4(%esp), %eax    /* récupère l'argument (adresse de gp) */
+    lgdt (%eax)           /* CPU : "ta GDT est ici maintenant" */
 
-    movw $0x10, %ax       /* kernel data - 0x10 */
+    /* Recharge les data segments depuis la nouvelle GDT */
+    movw $0x10, %ax       /* kernel data */
     movw %ax, %ds
     movw %ax, %es
     movw %ax, %fs
     movw %ax, %gs
 
-    movw $0x18, %ax       /* kernel STACK - 0x18 */
+    /* Recharge SS depuis la nouvelle GDT (vers le kernel stack) */
+    movw $0x18, %ax       /* kernel stack */
     movw %ax, %ss
+
+    /* Recharge CS via far jump — SEULE façon de changer CS */
+    ljmp $0x08, $.flush
 
 .flush:
     ret
